34b500a12a3ae78b09d4364716c9b8e27c0258c7
asamant2 <anisamanta@gmail.com>
2020-05-16 21:48:59 +0000

    Deploying to gh-pages from  @ 635c33e432963e9581a09990ecc7731521cf250a 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.43488fa41d82ce468a3845edbcac97ee.js"
+  "/precache-manifest.cb2d098259a8882eef351d6452e8e3f3.js"
 );
 
 self.addEventListener('message', (event) => {
